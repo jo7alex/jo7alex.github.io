97fe27715285c0a3ded8dc9dd4edeefa10a22a28
Vasanth.V <imvasanthv@gmail.com>
2020-04-04 23:03:35 +0530

    Added testimonies

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,65 @@
+const testimonies = [{
+	id: "testimony-jaason",
+	title: {
+		en: "Jaason - God has healed me from 3 years of sleeplessness - 2019",
+		ta: "ஜாஸன் - தேவன் 3 வருட தூக்கயின்மையிலிருந்து விடுதலை கொடுத்தார் - 2019"
+	},
+	photo: "jaason.jpeg",
+	content: {
+		en: "My name is Jaason. I was suffering from sleeplessness for the past 3 years. I will start running and screaming suddenly in the night while sleeping. Days passed. My sickness got worse and I got depression. In between I started a fruit shop. I faced a heavy loss in that too. With no sleep, no peace I lost the meaning of my life. One day I was sitting in my house thinking about the pathetic situation. Suddenly I remembered the Gospel preached by the Pastor in my village sometime back. So myself and my mother went to Anandur church. There, the Pastor and his wife prayed for me with compassion. They gave some food. I haven’t taken any food for past few days due to my sickness. But after they prayed for me, I ate all the food they gave. And I slept for hours in the church itself. Praise God. I prayed to God with all my heart. Thus me and my mother got the Salvation of Christ. Now, I also got the anointing of the Holy Spirit. Praise be to the name of Jesus.",
+		ta: "என் பெயர் ஜாசன். நான் 3 ஆண்டுகளுக்கு முன் இரவு தூங்க முடியாமல் மிகவும் அவதிப்பட்டேன். இரவு தூங்கும்போது திடிரென்று அலரிக்கொண்டு ஓடுவேன். நாட்கள் செல்ல செல்ல நான் தூங்காமல், எனக்கு மன அழுத்தம் வர துவங்கியது. இதன் இடையில் நான் ஒரு பழக்கடை வியாபாரம் ஆரம்பித்தேன். அதிலும் பெரிய நஷ்டம் ஏற்பட்டது. நான் தூக்கம் இல்லாதவனாய் மன நிம்மதி இழந்தவனாய் பித்து பிடித்தவனாய் வீட்டின் முன் அமர்ந்து இருக்கும் போது, ஒரு நாள் போதகர். பெஞ்சமின் எங்கள் ஊரில் வந்து இயேசு எல்லா பிரச்சனைகளில் இருந்தும் விடுதலை தருவார் என்று சொன்னது நினைவு வந்தது. உடனே என் தாயைக் கூட்டிக் கொண்டு, ஆனந்தூர் ஆலயத்திற்குச் சென்றேன். அங்கு போதகர் மற்றும் அவர் துணைவியார் எனக்காக ஜெபித்தார்கள். உடனே பல நாட்களாக சாப்பிட முடியாமல் இருந்த நான், நன்றாக சாப்பிட்டேன், நன்றாக தூங்கினேன். இயேசுவிடம் மனம் திறந்து பேசினேன். அவர் என் உள்ளத்தில் வந்தார். நானும் என் தாயாரும் இரட்ச்சிப்பைப் பெற்றுக் கொண்டோம். இப்போது நான் வேலை செய்கிறேன். பரிசுத்த ஆவியின் அபிஷேகத்தையும் அந்நிய பாஷை அடையாளத்தோடுப் பெற்றுக் கொண்டேன். தேவனுக்கே மகிமை உண்டாகட்டும்."
+	}
+}, {
+	id: "testimony-johnpeter",
+	title: {
+		en: "John Peter - God has delivered me from 25 years alcohol addiction - 2018",
+		ta: "ஜான் பீட்டர் - தேவன் 25 வருட குடி பழக்கத்திலிருந்து விடுதலை கொடுத்தார் - 2018"
+	},
+	photo: "kesavan.jpg",
+	content: {
+		en: "My name is Kesavan alias John Peter. I am 50 years old. My wife is Vasantha. And I have two sons. I was addicted to alcohol for the past 25 years. I work for daily wages and I spent all the money I earned on drinking alcohol. Even if I don’t have money in my hands, somehow I will drink alcohol by borrowing money from others. I have never slept without consuming alcohol. Because of irresponsible behavior, my sons and my wife stopped talking to me. But my wife still had a little hope for me and she prayed to God. All my church believers prayed for me in their prayers. Whenever I am drunk, I won’t allow my entire family to sleep peacefully and also I will not allow my village people to sleep peacefully. I will start a fight talking bad about others. So the next day people of my village will be complaining about me to my wife. This activity of mine hurted my family a lot. This was my life for past 25 years. At one point of time, I started losing vision in both my eyes and I suffered from severe stomach ulcer. I was not able to bear the pain. I was in my death bed. I lost all hope. But my wife said at that point, not to lose hope and trust in Jesus and surely he can give my life back. I believed her words and started going to church. God helped me to get out of alcoholic addiction. I started to read the bible and I got salvation. I got back my eye sight and my ulcer was healed by the pure Grace of God. Now my sons are in good job and I am a witness of the living God in my village. God also helped us to build our own house. Praise be to Jesus Christ.",
+		ta: "நான் கேசவன் என்ற ஜான் பீட்டர். எனக்கு இப்பொழுது 50 வயது. எனது மனைவி வசந்தா. எனக்கு இரண்டு மகன்கள் உள்ளனர். நான் கடந்த 25 ஆண்டுகள் குடிப்பழக்கத்திற்கு அடிமையாய் இருந்தேன். நான் ஒரு தினக்கூலி வேலை செய்பவன். அன்றன்று சம்பாதித்த பணத்தை முழுவதும் குடிப்பதற்கு மட்டும் செலவு செய்வேன். எனது கையில் பணம் இல்லையென்றாலும் கூட எப்படியாவது குடித்துவிட்டுத்தான் விட்டுக்கே வருவேன். ஒரு நாள் கூட குடிக்காமல் இருந்ததில்லை. இதனால் எப்போதும் குடும்பத்தில் ஏராளமான பிரச்சனைகள், சண்டைகள், நிம்மதி இல்லை. இதனால் என் மனைவி மற்றும் என் மகன்கள் என்னோடு பேசுவதை நிறுத்தி விட்டார்கள். ஆனால் என் மனைவியும் என் சபை, போதகர் மற்றும் மக்களும் எனக்காக அதிக கண்ணீரோடு ஜெபித்தார்கள். என்னைப் போல இந்த குடிப்பழத்திற்கு அடிமையானவர்கள் யாரும் இருக்க முடியாது. நான் குடித்துவிட்டு வரும்போதெல்லாம் அன்று இரவு முழுவதும் என் குடும்பத்தில் யாரையும் தூங்கவே விடமாட்டேன். நான் ஊரில் யாரையும் தூங்க விட மாட்டேன். அதிகாலை 5 மணிவரை சத்தம் போட்டுக் கொண்டே இருப்பேன். அல்லது வேண்டுமென்று மற்றவரை வம்பிழுத்து கொண்டே இருப்பேன். இதனால் மறுநாள் ஊர்மக்கள் எனது மனைவியிடம் புகார் செய்வார்கள். இந்த பிரச்சனை 25 ஆண்டு காலமாக தொடர்ந்தது. ஒரு கட்டத்தில் எனது இரண்டு கண்களும் பார்வை இழக்க ஆரம்பித்தது. அதிக குடிப் பழக்கத்தால் வயிறு வலியால் அவதிப்பட்டேன். மரணப்படுக்கை வரை சென்று விட்டேன். நான் ஏன் இந்த உலகத்தில் வாழ வேண்டும் என்ற எண்ணம் எனக்குள் வந்தது. என் மரணப் போராட்டத்தில், என் மனனவி என்னிடம், இயேசுவை விசுவாசி, கர்த்தர் உன்னை விடுவிப்பார் என்றாள். அப்பொழுது முதல் ஆனந்தூர் ஆலயத்திற்கு போக ஆரம்பித்தேன். கர்த்தரைத் தேடினேன். அவர் என்னை கைவிடவில்லை. என் ஜீவனை மீட்டுக் கொடுத்தார். குடிப்பழக்கத்தை முற்றிலும் மறந்தேன். மீண்டும் கண் பார்வை கிடைத்தது. வயிற்றுப் புண்ணும் சுகமானது. நான் இப்போது குடிப்பழக்கத்தை மறந்து இரண்டு வருடம் ஆகிறது. ஆண்டவர் எனக்கு சுகத்தையும், பெலனையும் கொடுத்தார். அதே போல் எனது மகன்களையும் ஆசீர்வதித்தார். சொந்த வீடு கட்டவும் கிருபை செய்தார். தேவனுக்கே மகிமை உண்டாவதாக."
+	}
+}, {
+	id: "testimony-mihavel",
+	title: {
+		en: "Mihavel - God has delivered us from the evil power and blessed us as a whole family - 2009",
+		ta: "மிகாவேல் - தேவன் எங்களை தீய வல்லமையில் இருந்து விடுதலை ஆக்கி குடும்பமாக ஆசீர்வதித்தார் - 2009"
+	},
+	photo: "mihavel.jpg",
+	content: {
+		en: "My name is Mihavel. Members in my family are my wife Victoria, my eldest son Joshua, my daughter Aroma and my youngest son Caleb. So we are a 5 member family. I accidentally read the crucifixion of Jesus Christ when I was not a believer and it touched me. But still I didn’t accept Jesus. When my children were small kids, one day suddenly they started running like people who lost their consciousness and they started throwing stones at others. We have taken them to doctors but they said that all the reports were normal. We went to a wizard but still no use. When my whole family was in pain, a priest in a temple asked us to go to the church to get deliverance from the bondage of evil power. So we went to HFJ Christ Church immediately. The Pastor and his wife started to pray for our children. And so as a family, we started confessing our sins and gave our hearts to The Lord Jesus Christ. Immediately my children were delivered miraculously. But I was not able to quit drinking habits. So I cried to Lord in my personal prayers and I was delivered from that bondage also. Now we are working in the church campus. Lord Jesus helped my children grow well, complete their studies and get good jobs. All Glory and Praise be to the name of The Lord Jesus Christ.",
+		ta: "என் பெயர் மிகாவேல். என் குடுபத்தில் என் மனைவி விக்டோரியா, என் மூத்த மகன் ஜோசுவா, மகள் அரோமா மற்றும் என் இளைய மகன் காலேப் ஆக மொத்தம் 5 நபர்கள். நான் ஆண்டவரை அறியாத போது, ஒரு நாள் பரிசுத்த வேதம் என் கைகளில் கிடைக்க, நான் அதில் இயேசுவின் பாடு மரணத்தை வாசிக்க நேர்ந்தது. அந்த வசனங்கள் என்னைத் தொட்டாலும், என் மனதில் இயேசுவுக்கு இடம் கொடுக்கவில்லை. நான் மதுபானம், புகைப் பிடித்தல், கஞ்சா புகைத்தல் போன்ற கொடிய பாவ பழக்கத்தில் இருந்து வந்தேன். என் பிள்ளைகள் சிறுப்பிள்ளைகாளாக இருந்த போது, திடிரென்று ஒரு நாள் என் மூன்று பிள்ளைகளும் பைத்தியம் பிடித்தவர்கள் போல் ஓடவும், மற்றவர்கள் மேல் கல்லெறியவும் தொடங்கினார்கள். மருத்துவர்களிடம் சென்றோம். ஒன்றும் இல்லை என்றே கூறினர். மந்திரவாதியிடம் சென்றோம். ஆனாலும் என் பிள்ளைகளுக்கு விடுதலை கிடைக்கவில்லை. நாங்கள் மன வேதனைக்குள் இருந்த அந்த நேரம், ஒரு பூசாரி, பிள்ளைகளை இயேசுவால் விடுவிக்க கூடும் என்றார். உடனே ஆனந்தூரில் உள்ள ஆலயதிற்கு போனோம். போதகர் மற்றும் அவர் மனைவி, பிள்ளைகளுக்காக ஜெபித்தார்கள். நாங்கள் குடும்பமாக பாவங்களை அறிக்கை செய்து இயேசுவுக்கு எங்கள் இருதயங்களை ஒப்புக்கொடுத்தோம். அற்புதமாக இயேசு என் பிள்ளைகளுக்கு விடுதலைக் கொடுத்தார். ஆனாலும் என் போதை பழக்கத்தை விட முடியாமல் தவித்தேன். ஆண்டவரே, உதவி செய்யும் என்று அழுதேன். அதில் இருந்தும் தேவன் அதிசயமாக விடுதலைக் கொடுத்தார். இப்போது தேவ ஆலயத்தில் அவர்களுக்கு உதவியாக குடும்பமாக பணி செய்கிறோம். என் பிள்ளைகளை தேவன் வளர்த்து, நல்ல ஆரோக்கியம், படிப்பு மற்றும் வேலையைக் கொடுத்து இருக்கிறார். தேவன் என் குடுபத்திற்கும் எனக்கும் செய்த நன்மைகளுக்காக இயேசு கிறிஸ்துவுக்கே மகிமை உண்டாகட்டும்."
+	}
+}, {
+	id: "testimony-moses",
+	title: {
+		en: "Moses - God has blessed us with children - 2006",
+		ta: "மோசே - தேவன் குழந்தைகளை கொடுத்து ஆசீர்வதித்தார் - 2006"
+	},
+	photo: "moses.jpg",
+	content: {
+		en: "My name is Moses and my wife is Shiney. Few years back, when I don’t know about Jesus, We had a girl child. But she died in few days after her birth. Then we had a boy child. Same as the first child, the second child got sick after a few days of his birth. I went to many religious places seeking for help. Doctors also told that they couldn’t save him. One day, he died in my hands. I was shattered. At that time I came to know about Jesus Christ and I went to HFJ Christ Church. The Lord touched my heart and we accepted Jesus as our Saviour. My wife got conceived and delivered a healthy girl baby in the next 7th months. Again we had a second girl baby. We prayed to God for a boy child. God has given us a boy child. He was 7 months born preterm child. And so he was in critical health condition and the doctors said they could not save him. We submitted our son Joshua in God’s hand and prayed. Even the doctors got surprised when my son was saved by Jesus Christ. All my children are good and doing their schoolings now. Praise be to The Lord Jesus Name alone.",
+		ta: "என்னுடைய பெயர் மோசே. என் மனைவி பெயர் ஷைனி. சில வருடங்களுக்கு முன், நான் இயேசுவை அறியாத போது, எனக்கு ஒரு குழந்தை பிறந்து இறந்து விட்டது. அதன் பின் சில மாதங்களில் ஒரு ஆண் குழந்தை பிறந்தது. ஆனால் சில நாட்களில் அதுவும் நோயில் விழுந்தது. நான் எல்லா இடங்களுக்கும் பிள்ளையை தூக்கிச் சென்று வேண்டினேன். ஆனால் என் கையில் இருக்கும்போதே பிள்ளை மரித்துப் போனது. அப்போது ஒரு நாள் இயேசுவைப் பற்றி கேள்விப் பட்டு, ஆனந்தூர் ஆலயதிற்க்குச் சென்றோம். அங்கு இயேசுவை எங்கள் சொந்த இரட்சகராக ஏற்றுக் கொண்டோம். அடுத்த 7 மாதத்தில் ஒரு பெண் குழந்தை நல்ல ஆரோக்கியமாக பிறந்தது. தேவனுக்கே மகிமை. மீண்டும் ஒரு பெண் குழந்தைப் பிறந்தது. மீண்டும் ஒரு ஆண் குழந்தைக்காக ஜெபித்தோம். ஒரு ஆண் பிள்ளை பிறந்தது. ஆனால் 7 மாதத்தில் பிறந்து, மருத்துவர்களால் கைவிடப்பட்ட சூழ்நிலையில் தேவனிடம் குழந்தையை ஒப்புக்கொடுத்து கண்ணீரோடு ஜெபித்தோம். மருத்துவர்கள் வியக்கும்படி என் மகன் அற்புத சுகம் பெற்றான். இப்போது என் மூன்று பிள்ளைகளும் பள்ளியில் படித்துக் கொண்டிருக்கிறார்கள். இயேசு கிறிஸ்துவின் நாமம் மட்டுமே மகிமை படட்டும்."
+	}
+}, {
+	id: "testimony-peter",
+	title: {
+		en: "Peter - God has blessed us with children - 1998",
+		ta: "பீட்டர் - தேவன் குழந்தை பாக்கியத்தை அருளினார் - 1998"
+	},
+	photo: "peter.jpg",
+	content: {
+		en: "I am Peter and my wife is Selvi. We got married in the year of 1985. We don’t have any children for more than 10 years. We went to many religious places and we did a lot of rituals, none of those helped. We went to many hospitals for treatment, but were upset to hear doctors say we cant get conceived. We also tried siddha medicines. But all our efforts were in vain. Then we came to know about Lord Jesus Christ by one of our relatives. He told that if we ask anything faithfully in Jesus name it will be given to us. So we started to pray with faith. We confessed our sins and got salvation. Since then my wife will be conceived, but she will get a nightmare of a serpent hissing and will cause a miscarriage. But we and our Pastor’s family prayed to God with faith. Seeing our tearful prayers God has given a girl child in the year of 1998 after 13 long years. After that, God also given us two more daughters. All my daughters have grown up and doing their higher studies in medical related courses. All Glory be to Jesus for answering our prayers.",
+		ta: "என் பெயர் பீட்டர். என் மனைவி பெயர் செல்வி. எங்களுக்கு 1985 ஆம் ஆண்டு திருமணம் ஆனது. எங்களுக்கு திருமணமாகி 10 வருடங்களுக்குமேல் குழந்தை இல்லை. பல இடங்களுக்கு சென்று பல பரிகாரங்கள் செய்தோம். பல மருத்துவமனைகளுக்கு சென்றோம். எங்களுக்கு குழந்தை பிறக்க வாய்ப்பே இல்லை என்றே கூறினார்கள் மருத்துவர்கள். கடுமையான சித்த மருத்துவங்களைப் பின் பற்றிப் பார்த்தும் பலன் இல்லை. இந்த சூழ்நிலையில் இயேசுவை விசுவாசித்தால் குழந்தை பிறக்கும் என்று என் உறவினர் மூலம் கேள்விப் பட்டு கடைசி நம்பிக்கையாக ஆனந்தூர் ஆலயத்திற்குச் சென்றோம். இயேசுவை எங்கள் உள்ளத்தில் ஏற்றுக் கொண்டு அறிக்கை செய்து இரட்சிக்கப்பட்டோம். அதன் பின் என் மனைவி கருவுறுவாள், பாம்பு சீறுவது போல் கனவு வரும், கரு கலைந்துவிடும். ஆனால் நாங்களும் போதகர் குடும்பமும் விசுவாசத்தை விடாமல் உறுதியாய் ஜெபித்தோம். எங்கள் கண்ணீர் ஜெபத்திற்கு தேவன் அற்புதமாக பதில் அளித்தார். 13 ஆண்டுகளுக்கு பிறகு 1998 ஆம் ஆண்டு ஒரு பெண் குழந்தையை கொடுத்தார். அதன் பிறகு இரண்டு பெண் குழந்தைகளையும் கொடுத்தார் தேவன். இப்போது என் மூன்று பிள்ளைகளும் மருத்துவ துறை சார்ந்த மேல் படிப்புகளில் படித்துக் கொண்டிருக்கிறார்கள். தேவன் என்னையும் என் குடும்பத்தையும் ஆசீர்வதித்திருக்கிறார். இயேசுவின் நாமம் மகிமைப்படட்டும்."
+	}
+}]
+
 const hfjLang = new Vue({
 	el: '#hfj-site',
 	data: {
-		lang: window.localStorage.lang || "ta"
+		lang: window.localStorage.lang || "ta",
+		testimonies: testimonies
 	},
 	methods: {
 		setLang: function(lang) {
@@ -9,4 +67,7 @@ const hfjLang = new Vue({
 			this.lang = lang;
 		}
 	}
-})
+});
+// setTimeout(() => {
+// 	$('.collapse').collapse();
+// }, 2000)
